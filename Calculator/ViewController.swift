bfafa83cb3a94b86a98c65e0599b5aa003b0cbd4
Evgeniy Kurinnoy <ekurinnoy@gmail.com>
2019-06-16 17:19:07 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Евгений Куринной on 6/16/19.
+//  Copyright © 2019 Evgeniy Kurinnoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
